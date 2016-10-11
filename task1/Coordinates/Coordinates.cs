7daa5f0413b080a7d6fbec5efb8b1ff2a10161d2
Igor Dubina <igarek2706@mail.ru>
2016-10-11 23:22:43 +0300

    added task1

---
+++ task1/Coordinates/Coordinates.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CoordinatesLibrary
+{
+    public class Coordinates
+    {
+        private static bool IsCorrectInput(string[] array)
+        {
+            foreach (string s in array)
+            {
+                double tmp = 0;
+                bool result = Double.TryParse(s.Replace('.', ','), out tmp);  // !
+                if (!result) return false;
+            }
+            return true;
+        }
+
+        public static bool ReadFromFile(string path)
+        {
+            StreamReader sr = File.OpenText(path);
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+
+                string[] fields = line.Split(',');
+
+                if (!IsCorrectInput(fields))
+                {
+                    Console.WriteLine("Invalid input");
+                    return false;
+                }
+
+                try
+                {
+                    Console.WriteLine(String.Format("X:{0} Y:{1}", fields[0].Replace('.', ','), fields[1].Replace('.', ',')));
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine(string.Format("{0}", ex.Message));
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
---
+++ task1/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoordinatesLibrary;
+ 
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "input.txt";
+           Coordinates.ReadFromFile(path);   
+        }
+    }
+}
